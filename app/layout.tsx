541324168219678e970e13f74570c15343a2e3da
Sas1694 <sergioalvsam@hotmail.com>
2026-05-13 20:47:11 +0200

    feat: remove IosTouchActiveFix component to streamline layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { getLocale } from "next-intl/server";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { IosTouchActiveFix } from "@/components/IosTouchActiveFix";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,7 +23,6 @@ export default async function RootLayout({
   return (
     <html lang={locale} className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col bg-[hsl(30,20%,97%)]">
-        <IosTouchActiveFix />
         {children}
         <Analytics />
         <SpeedInsights />
